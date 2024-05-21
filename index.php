82597070baca408e41ae7903f1d0625c19661d8d
ane-k-goncalves <goncalvesanne961@gmail.com>
2024-05-21 01:50:32 -0300

    funções vender, verificarLog, menu, e modificações implementadas

--- index.php
+++ index.php
@@ -1,42 +1,109 @@
 <?php
-    $users = [];
+
+    $users = ['user' => 'admin', 'password' => 'password'];
+
+    $vendas = 0;
+
+    $logs = [];
+
+    function logs($mensagem) {
+        global $logs;
+
+        $logs[] = "[" . date('s/m/Y H:i:s') . "]" . $mensagem;
+    }
 
 
     function login($users){
-        $user = readline("nome: ");
+        $username = readline("nome: ");
         $password = readline("senha: ");
 
         global $users;
+        global $logs;
 
-        //arrumar -> não verifica se 
-            foreach ($users as $userData ) {
-            if ($userData['user'] === $user && $userData['password'] === $password) {
-                return "Bem-vindo!";
-            }else {
-                return "Usuário não encontrado.";
+            if (isset ($users['user']) && $users['password'] == $password){
+                logs("Bem-vindo $username! \n");
+                echo "Bem-vindo $username! \n";
+                return true;
             }
+        
+        echo "Usuário não encontrado. \n";
+        return false;
         }
-        return;
-    }
         
     
-    function cadastrar(){
-        $user = readline("nome: ");
+    function cadastrar($users){
+        $username = readline("nome: ");
         $password = readline("senha: ");
-    
+
         global $users;
+        global $logs;
     
-        $users[] = ["user" => $user, "password" => $password];
+        $users[] = ["user" => $username, "password" => $password];
     
+        logs("Usuário $username cadastrado. \n");
         echo "Usuário cadastrado.\n";
+        return true;
     
+    }
+    
+    function vender(){
+        global $vendas;
+
+        $item = readline("Qual o nome do item vendido: ");
+        $valor = readline("Qual o valor do item: ");
+
+        $vendas += $valor;
+
+        logs("O usuário vendeu o item $item no valor de R$ $valor. ");
+
+        return $vendas . "Venda realizada! \n";
+    }
+
+    function verificarLog($logs){
+        global $logs;
+
+        foreach($logs as $log){
+            print_r($logs) . "\n";
         }
     
+    }
+
+    function menu(){
+        while(true){
+
+            global $users;
+            global $logs;
+
+            echo "Digite \n1.Vender \n2.Cadastro Novo usuário \n3.Verificar log \n4.Sair \n";
+
+            $acao = readline("Escolha: ");
+
+            switch($acao){
+                case 1:
+                    vender();
+                    break;
+                case 2:
+                    cadastrar($users);
+                    break;
+                case 3:
+                    verificarLog($logs);
+                    break;
+                case 4:
+                    die();
+                    break;
+            
+            }
+        }
+    }
+
+   
 
     
-    $x = 0;
+    function loja(){
+    
+    global $users;
 
-    while($x == 0) {   //usar um looping
+    while(true) {   //usar um looping
         echo "Digite \n1.Entrar \n2.Cadastrar \n";
 
         $a  = readline("Escolha: ");
@@ -46,53 +113,33 @@
 
             $a  = readline("Escolha: ");
 
-            switch($a){
-                case 1:
+                if($a == 1) {
                     login($users);
-                    break;
-                case 2:
-                    die();
-                    break;
-            }
+                    menu();
+            
+                } else if ($a == 2){
 
-            if($a == 1) {
+                die();
+             }
 
-                echo "Digite \n1.Vender \n2.Cadastro Novo usuário \n3.Verificar log \n4.Sair \n";
+             } else if ($a == 2){
 
-                $acao = readline("Escolha: ");
+                echo "Digite \n1.Cadastrar \n2.Sair \n";
 
-                switch($acao){
+                $a  = readline("Escolha: ");
+
+                switch($a){
                     case 1:
-                        vender();
+                        cadastrar($users);
                         break;
                     case 2:
-                        cadastrar();
-                        break;
-                    case 3:
-                        verificarLog();
-                        break;
-                    case 4:
                         die();
                         break;
-                            //arrumar para esseeeeeeeeee ser while
                 }
             }
-
-        }else if($a == 2){
-
-            echo "Digite \n1.Cadastrar \n2.Sair \n";
-
-            $a  = readline("Escolha: ");
-
-            switch($a){
-                case 1:
-                    cadastrar();
-                    break;
-                case 2:
-                    die();
-                    break;
-            }
-        }
-    }  
+        }  
+    }
+    
 
 
+loja();
